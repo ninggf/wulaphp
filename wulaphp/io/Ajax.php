fd309288a45ec607e8df50a2cc1d50ecd696a07f
windywany <windywany@163.com>
2017-03-27 11:17:54 +0800

    add style field to ajax response

--- wulaphp/io/Ajax.php
+++ wulaphp/io/Ajax.php
@@ -12,11 +12,19 @@ use wulaphp\mvc\view\JsonView;
  *      code:200,
  *      action:'',
  *      message: '',
+ *      style:'',
  *      target:'',
  *      args: {}
  *  }
  *
- *  code 与 action 是肯定有的，message,target,args根据不同的action而不同.
+ *  code 与 action 是肯定有的，message,target,args根据不同的action而不同,style定义message的显示样式.
+ *  message 可以有两种格式：
+ *  1. 字符串
+ *  2. 对象
+ *     {
+ *          "title":"消息提示标题",
+ *          "message":"消息正文"
+ *      }
  *
  * @author  Leo Ning <windywany@gmail.com>
  * @package wulaphp\io
@@ -26,7 +34,6 @@ class Ajax {
 	const         ERROR        = 300;
 	const         WARNING      = 400;
 	const         INFO         = 500;
-	const         ACT_TIP      = 'tip';//提示信息
 	const         ACT_CALLBACK = 'callback';//回调js函数.
 	const         ACT_DIALOG   = 'dialog';//弹出对话框
 	const         ACT_UPDATE   = 'update';//更新元素
@@ -39,58 +46,63 @@ class Ajax {
 	/**
 	 * 成功.
 	 *
-	 * @param string $message
+	 * @param string|array $message 提示消息.
+	 * @param int          $style   显示样式.
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function success($message) {
-		return new JsonView(['code' => self::SUCCESS, 'message' => $message, 'action' => self::ACT_TIP]);
+	public static function success($message, $style = null) {
+		return new JsonView(['code' => self::SUCCESS, 'message' => $message, 'sytle' => $style]);
 	}
 
 	/**
 	 * 失败.
 	 *
-	 * @param string $message
+	 * @param string|array $message
+	 * @param int          $style 显示样式.
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function error($message) {
-		return new JsonView(['code' => self::ERROR, 'message' => $message, 'action' => self::ACT_TIP]);
+	public static function error($message, $style = null) {
+		return new JsonView(['code' => self::ERROR, 'message' => $message, 'sytle' => $style]);
 	}
 
 	/**
 	 * 警告.
 	 *
-	 * @param string $message
+	 * @param string|array $message
+	 * @param int          $style 显示样式.
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function warn($message) {
-		return new JsonView(['code' => self::WARNING, 'message' => $message, 'action' => self::ACT_TIP]);
+	public static function warn($message, $style = null) {
+		return new JsonView(['code' => self::WARNING, 'message' => $message, 'sytle' => $style]);
 	}
 
 	/**
 	 * 提示.
 	 *
-	 * @param string $message
+	 * @param string|array $message
+	 * @param string       $style 显示样式.
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function info($message) {
-		return new JsonView(['code' => self::INFO, 'message' => $message, 'action' => self::ACT_TIP]);
+	public static function info($message, $style = null) {
+		return new JsonView(['code' => self::INFO, 'message' => $message, 'sytle' => $style]);
 	}
 
 	/**
 	 * 调用js函数.
 	 *
-	 * @param string $func    要调用的js函数
-	 * @param array  $args    参数
-	 * @param string $message 提示信息
+	 * @param string       $func    要调用的js函数
+	 * @param array        $args    参数
+	 * @param string|array $message 提示信息
+	 * @param string       $style
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function callback($func, $args = [], $message = '') {
-		return new JsonView(['code' => self::SUCCESS, 'message' => $message, 'target' => $func, 'args' => $args, 'action' => self::ACT_CALLBACK]);
+	public static function callback($func, $args = [], $message = '', $style = null) {
+		return new JsonView(['code' => self::SUCCESS, 'message' => $message, 'sytle' => $style, 'target' => $func, 'args' => $args, 'action' => self::ACT_CALLBACK]);
 	}
 
 	/**
@@ -113,82 +125,88 @@ class Ajax {
 	/**
 	 * 更新元素内容.
 	 *
-	 * @param string $target  css selector
-	 * @param string $content html
-	 * @param string $message 提示信息.
-	 * @param bool   $append  是否是追加内容.
+	 * @param string       $target  css selector
+	 * @param string       $content html
+	 * @param string|array $message 提示信息.
+	 * @param bool         $append  是否是追加内容.
+	 * @param string       $style
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function update($target, $content, $message = '', $append = false) {
+	public static function update($target, $content, $message = '', $append = false, $style = null) {
 		$args = ['content' => $content, 'append' => $append];
 
-		return new JsonView(['code' => self::SUCCESS, 'target' => $target, 'message' => $message, 'args' => $args, 'action' => self::ACT_UPDATE]);
+		return new JsonView(['code' => self::SUCCESS, 'target' => $target, 'message' => $message, 'sytle' => $style, 'args' => $args, 'action' => self::ACT_UPDATE]);
 	}
 
 	/**
 	 * 重新加载.
 	 *
-	 * @param string $target  css selector
-	 * @param string $message message
+	 * @param string       $target  css selector
+	 * @param string|array $message message
+	 * @param string       $style
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function reload($target, $message = '') {
+	public static function reload($target, $message = '', $style = null) {
 
-		return new JsonView(['code' => self::SUCCESS, 'target' => $target, 'message' => $message, 'action' => self::ACT_RELOAD]);
+		return new JsonView(['code' => self::SUCCESS, 'target' => $target, 'message' => $message, 'sytle' => $style, 'action' => self::ACT_RELOAD]);
 	}
 
 	/**
 	 * 点击元素.
 	 *
-	 * @param string $target  css selector
-	 * @param string $message message
+	 * @param string       $target  css selector
+	 * @param string|array $message message
+	 * @param string       $style
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function click($target, $message = '') {
+	public static function click($target, $message = '', $style = null) {
 
-		return new JsonView(['code' => self::SUCCESS, 'target' => $target, 'message' => $message, 'action' => self::ACT_CLICK]);
+		return new JsonView(['code' => self::SUCCESS, 'target' => $target, 'message' => $message, 'sytle' => $style, 'action' => self::ACT_CLICK]);
 	}
 
 	/**
 	 * 重定向.
 	 *
-	 * @param string $url
-	 * @param string $message
+	 * @param string       $url
+	 * @param string|array $message
+	 * @param string       $style
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function redirect($url, $message = '') {
+	public static function redirect($url, $message = '', $style = null) {
 
-		return new JsonView(['code' => self::SUCCESS, 'target' => $url, 'message' => $message, 'action' => self::ACT_REDIRECT]);
+		return new JsonView(['code' => self::SUCCESS, 'target' => $url, 'message' => $message, 'sytle' => $style, 'action' => self::ACT_REDIRECT]);
 	}
 
 	/**
 	 * 表单验证出错.
 	 *
-	 * @param string $target  表单.
-	 * @param array  $errors  错误信息.
-	 * @param string $message 提示信息.
+	 * @param string       $target  表单.
+	 * @param array        $errors  错误信息.
+	 * @param string|array $message 提示信息.
+	 * @param string       $style
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function validate($target, $errors, $message = '') {
+	public static function validate($target, $errors, $message = '', $style = null) {
 		$args = ['errors' => $errors];
 
-		return new JsonView(['code' => self::WARNING, 'target' => $target, 'message' => $message, 'args' => $args, 'action' => self::ACT_VALIDATE]);
+		return new JsonView(['code' => self::WARNING, 'target' => $target, 'message' => $message, 'sytle' => $style, 'args' => $args, 'action' => self::ACT_VALIDATE]);
 	}
 
 	/**
 	 * 直接运行$script.
 	 *
-	 * @param string $script
-	 * @param string $message
+	 * @param string       $script
+	 * @param string|array $message
+	 * @param string       $style
 	 *
 	 * @return \wulaphp\mvc\view\JsonView
 	 */
-	public static function script($script, $message = '') {
-		return new JsonView(['code' => self::SUCCESS, 'target' => $script, 'message' => $message, 'action' => self::ACT_SCRIPT]);
+	public static function script($script, $message = '', $style = null) {
+		return new JsonView(['code' => self::SUCCESS, 'target' => $script, 'message' => $message, 'sytle' => $style, 'action' => self::ACT_SCRIPT]);
 	}
 }
