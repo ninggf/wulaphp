b3c34de10b0894043205e3638a88b9122820da22
windywany <windywany@163.com>
2017-04-27 10:25:24 +0800

    过滤${}

--- wulaphp/mvc/view/MustacheFilter.php
+++ wulaphp/mvc/view/MustacheFilter.php
@@ -4,10 +4,10 @@ namespace wulaphp\mvc\view;
 
 class MustacheFilter {
 	public function pre($tpl_source, \Smarty_Internal_Template $template) {
-		return str_replace(['{{', '}}', '@{'], ['x { y { z', 'x } y } z', '@ { @'], $tpl_source);
+		return str_replace(['{{', '}}', '@{','${'], ['x { y { z', 'x } y } z', '@ { @','- { -'], $tpl_source);
 	}
 
 	public function post($tpl_source, \Smarty_Internal_Template $template) {
-		return str_replace(['x { y { z', 'x } y } z', '@ { @'], ['{{', '}}', '{'], $tpl_source);
+		return str_replace(['x { y { z', 'x } y } z', '@ { @','- { -'], ['{{', '}}', '{','${'], $tpl_source);
 	}
 }
