ac2ad852b38d73a888908982227309cfb3acf63d
windywany <windywany@163.com>
2018-02-27 11:09:49 +0800

    add noquote method

--- wulaphp/db/sql/ImmutableValue.php
+++ wulaphp/db/sql/ImmutableValue.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace wulaphp\db\sql;
 
 use wulaphp\db\dialect\DatabaseDialect;
@@ -10,31 +11,72 @@ use wulaphp\db\dialect\DatabaseDialect;
  *
  */
 class ImmutableValue {
-
 	private $value;
-
 	private $alias;
+	private $nquote = false;
 	/**
 	 * @var DatabaseDialect
 	 */
 	private $dialect;
 
+	/**
+	 * ImmutableValue constructor.
+	 *
+	 * @param string $value
+	 * @param null   $alias
+	 */
 	public function __construct($value, $alias = null) {
-		$this->value = $value;
+		$this->value = (string)$value;
 		$this->alias = $alias;
 	}
 
+	/**
+	 * 数据库驱动.
+	 *
+	 * @param \wulaphp\db\dialect\DatabaseDialect $dialect
+	 *
+	 * @return \wulaphp\db\sql\ImmutableValue
+	 */
 	public function setDialect(DatabaseDialect $dialect) {
 		$this->dialect = $dialect;
+
+		return $this;
 	}
 
+	/**
+	 * 别名.
+	 *
+	 * @param string $alias
+	 *
+	 * @return \wulaphp\db\sql\ImmutableValue
+	 */
 	public function alias($alias) {
 		$this->alias = $alias;
+
+		return $this;
+	}
+
+	/**
+	 * 不转义字符.
+	 *
+	 * @return \wulaphp\db\sql\ImmutableValue
+	 */
+	public function noquote() {
+		$this->nquote = true;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getValue() {
+		return $this->__toString();
 	}
 
 	public function __toString() {
 		$dialect = $this->dialect;
-		if ($dialect == null) {
+		if ($dialect == null || $this->nquote) {
 			$value = $this->value;
 		} else {
 			$value = trim($dialect->quote($this->value), "'");
