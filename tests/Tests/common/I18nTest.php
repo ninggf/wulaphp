20a50a9231f011f399ca302cdbe0fa095afa8559
windywany <windywany@163.com>
2018-09-28 17:45:44 +0800

    添加更多测试

--- tests/Tests/common/I18nTest.php
+++ tests/Tests/common/I18nTest.php
@@ -63,4 +63,39 @@ class I18nTest extends TestCase {
         $str1 = I18n::translate1('step', [5], '@validator');
         self::assertEquals($str, $str1);
     }
+
+    /**
+     * @depends testLang
+     */
+    public function testFuncI18n() {
+        //以目录组织且文件存在
+        $file = _i18n('/assets/user.js');
+        $this->assertEquals('<script type="text/javascript" src="/assets/user/en.js"></script>', $file);
+
+        $file = _i18n('/assets/user.css');
+        $this->assertEquals('<link rel="stylesheet" href="/assets/user/en.css"/>', $file);
+
+        $file = _i18n('/assets/user.png');
+        $this->assertEquals('/assets/user/en.png', $file);
+
+        //文件不存在
+        $file = _i18n('/assets/avatar.js');
+        $this->assertEquals('<script type="text/javascript" src="/assets/avatar_en.js"></script>', $file);
+
+        $file = _i18n('/assets/avatar.css');
+        $this->assertEquals('<link rel="stylesheet" href="/assets/avatar_en.css"/>', $file);
+
+        $file = _i18n('/assets/avatar.png');
+        $this->assertEquals('/assets/avatar_en.png', $file);
+
+        //指定扩展名
+        $file = _i18n('/assets/avatar', '.js');
+        $this->assertEquals('<script type="text/javascript" src="/assets/avatar_en.js"></script>', $file);
+
+        $file = _i18n('/assets/avatar', '.css');
+        $this->assertEquals('<link rel="stylesheet" href="/assets/avatar_en.css"/>', $file);
+
+        $file = _i18n('/assets/avatar', '.png');
+        $this->assertEquals('/assets/avatar_en.png', $file);
+    }
 }
