bf64407ac8ed6390e383d37c8d0bb0b435b6d64f
windywany <windywany@163.com>
2018-11-08 16:01:42 +0800

    调整默认模块分发器

--- wulaphp/router/DefaultModuleDispatcher.php
+++ wulaphp/router/DefaultModuleDispatcher.php
@@ -13,19 +13,26 @@ namespace wulaphp\router;
 use wulaphp\app\App;
 
 class DefaultModuleDispatcher implements IURLDispatcher {
-	private $dd = null;
-	private $defaultModule;
+    private $dd = null;
+    private $defaultModule;
 
-	public function __construct(IURLDispatcher $dispatcher,  $defaultModule) {
-		$this->dd            = $dispatcher;
-		$this->defaultModule = App::id2dir($defaultModule);
-	}
+    public function __construct(IURLDispatcher $dispatcher, $defaultModule) {
+        $this->dd            = $dispatcher;
+        $this->defaultModule = App::id2dir($defaultModule);
+    }
 
-	public function dispatch($url, $router, $parsedInfo) {
-		if ($url == 'index.html') {
-			$url = '';
-		}
+    public function dispatch($url, $router, $parsedInfo) {
+        if ($url == 'index.html') {
+            $url = '';
+        }
+        if ($url) {
+            $urls = explode('/', $url);
+            $id   = App::dir2id($urls[0]);
+            if ($id) {
+                return null;
+            }
+        }
 
-		return $this->dd->dispatch(untrailingslashit($this->defaultModule . '/' . $url), $router, $parsedInfo);
-	}
+        return $this->dd->dispatch(untrailingslashit($this->defaultModule . '/' . $url), $router, $parsedInfo);
+    }
 }
