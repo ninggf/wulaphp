579218fc868ea0ce7bfa22991a3660a1d6dd16d5
windywany <windywany@163.com>
2018-11-21 18:19:13 +0800

    修复查询条件不起作用BUG

--- wulaphp/form/providor/TableDataProvidor.php
+++ wulaphp/form/providor/TableDataProvidor.php
@@ -19,145 +19,150 @@ use wulaphp\validator\JQueryValidator;
  */
 class TableDataProvidor extends FieldDataProvidor {
 
-	public function getData($search = false) {
-		$options = $this->optionAry;
-		if (!isset($options['table']) && !isset($options['tableName'])) {
-			return [];
-		}
-		$table = isset($options['table']) ? $options['table'] : $options['tableName'];
-		$where = isset($options['where']) ? $options['where'] : [];
-		$keyId = isset($options['key']) ? $options['key'] : 'id';
-		$field = isset($options['fields']) ? $options['fields'] : 'name';
-		$eval  = isset($options['eval']);
-		$sort  = isset($options['orderBy']) ? $options['orderBy'] : '';
-		if ($sort && !is_array($sort)) {
-			$sorts = explode(';', $sort);
-			$sort  = [];
-			foreach ($sorts as $ss) {
-				$sort[] = explode(' ', $ss);
-			}
-		}
-		$option = isset($options['option']) ? $options['option'] : [];
-		$pid    = isset($options['pid']) ? $options['pid'] : false;
-		if ($option && !is_array($option)) {
-			$option = ['' => $option];
-		}
-		$format = method_exists($this->form, 'formatData');
-		if ($where && $eval) {
-			$tableData = $this->form->tableData();
-			$dataKeys  = array_keys($tableData);
-			$dataVals  = array_values($tableData);
-			array_walk($dataKeys, function (&$item) {
-				$item = '{' . $item . '}';
-			});
-			foreach ($where as $key => $value) {
-				$where[ $key ] = str_replace($dataKeys, $dataVals, $value);
-			}
-		}
-		$table = new SimpleTable($table);
-		if ($format) {
-			$datas = $table->select($keyId . ',' . $field);
-			if ($sort) {
-				$datas->sort($sort);
-			}
-			$datas     = $datas->toArray(null, $keyId);
-			$fieldName = $this->field->getName();
-			foreach ($datas as $k => $v) {
-				$datas[ $k ] = $this->form->formatData($fieldName, $v);
-			}
+    public function getData($search = false) {
+        $options = $this->optionAry;
+        if (!isset($options['table']) && !isset($options['tableName'])) {
+            return [];
+        }
+        $table = isset($options['table']) ? $options['table'] : $options['tableName'];
+        $where = isset($options['where']) ? $options['where'] : [];
+        $keyId = isset($options['key']) ? $options['key'] : 'id';
+        $field = isset($options['fields']) ? $options['fields'] : 'name';
+        $eval  = isset($options['eval']);
+        $sort  = isset($options['orderBy']) ? $options['orderBy'] : '';
+        if ($sort && !is_array($sort)) {
+            $sorts = explode(';', $sort);
+            $sort  = [];
+            foreach ($sorts as $ss) {
+                $sort[] = explode(' ', $ss);
+            }
+        }
+        $option = isset($options['option']) ? $options['option'] : [];
+        $pid    = isset($options['pid']) ? $options['pid'] : false;
+        if ($option && !is_array($option)) {
+            $option = ['' => $option];
+        }
+        $format = method_exists($this->form, 'formatData');
+        if ($where && $eval) {
+            $tableData = $this->form->tableData();
+            $dataKeys  = array_keys($tableData);
+            $dataVals  = array_values($tableData);
+            array_walk($dataKeys, function (&$item) {
+                $item = '{' . $item . '}';
+            });
+            foreach ($where as $key => $value) {
+                $where[ $key ] = str_replace($dataKeys, $dataVals, $value);
+            }
+        }
+        $table = new SimpleTable($table);
+        if ($format) {
+            $datas = $table->select($keyId . ',' . $field);
+            if ($sort) {
+                $datas->sort($sort);
+            }
+            $datas     = $datas->where($where)->toArray(null, $keyId);
+            $fieldName = $this->field->getName();
+            foreach ($datas as $k => $v) {
+                $datas[ $k ] = $this->form->formatData($fieldName, $v);
+            }
 
-			return $datas;
-		} else if ($pid && $option) {
-			$datas = $table->select($keyId . ',' . $field);
-			if ($sort) {
-				$datas->sort($sort);
-			}
-			$opts = $option;
-			$datas->treeKey('id')->tree($opts, $keyId, $pid);
+            return $datas;
+        } else if ($pid && $option) {
+            $datas = $table->select($keyId . ',' . $field);
+            if ($sort) {
+                $datas->sort($sort);
+            }
+            $opts = $option;
+            $datas->where($where)->treeKey('id')->tree($opts, $keyId, $pid);
 
-			return $opts;
-		} else {
-			$datas = $table->select($keyId . ',' . $field);
-			if ($sort) {
-				$datas->sort($sort);
-			}
-			if ($option) {
-				return $datas->toArray($field, $keyId, $option);
-			} else {
-				return $datas->toArray($field, $keyId);
-			}
-		}
-	}
+            return $opts;
+        } else {
+            $datas = $table->select($keyId . ',' . $field)->where($where);
+            if ($sort) {
+                $datas->sort($sort);
+            }
+            if ($option) {
+                return $datas->toArray($field, $keyId, $option);
+            } else {
+                return $datas->toArray($field, $keyId);
+            }
+        }
+    }
 
-	public function createConfigForm() {
-		$form = new TableDataProvidorForm(true);
-		$form->inflateByData($this->optionAry);
+    public function createConfigForm() {
+        $form = new TableDataProvidorForm(true);
+        $form->inflateByData($this->optionAry);
 
-		return $form;
-	}
+        return $form;
+    }
 }
 
+/**
+ * Class TableDataProvidorForm
+ * @package wulaphp\form\providor
+ * @internal
+ */
 class TableDataProvidorForm extends FormTable {
-	use JQueryValidator;
-	public $table = null;
-	/**
-	 * 表名
-	 * @var \backend\form\TextField
-	 * @type string
-	 * @required
-	 * @layout 1,col-xs-4
-	 */
-	public $tableName;
-	/**
-	 * 值字段
-	 * @var  \backend\form\TextField
-	 * @type string
-	 * @required
-	 * @layout 1,col-xs-4
-	 */
-	public $key = 'id';
-	/**
-	 * 文本字段
-	 * @var \backend\form\TextField
-	 * @type string
-	 * @required
-	 * @layout 1,col-xs-4
-	 */
-	public $fields = 'name';
-	/**
-	 * 条件
-	 * @var \backend\form\TextField
-	 * @type string
-	 * @layout 2,col-xs-12
-	 */
-	public $where;
-	/**
-	 * 解析条件中的变量
-	 * @var \backend\form\CheckboxField
-	 * @type bool
-	 * @layout 3,col-xs-12
-	 */
-	public $eval = 0;
-	/**
-	 * 排序
-	 * @var \backend\form\TextField
-	 * @type string
-	 * @note   格式: field a,field2 d
-	 * @layout 4,col-xs-12
-	 */
-	public $orderBy;
-	/**
-	 * 默认选项提示文字
-	 * @var \backend\form\TextField
-	 * @type string
-	 * @layout 5,col-xs-8
-	 */
-	public $option;
-	/**
-	 * 树型选项父字段
-	 * @var \backend\form\TextField
-	 * @type string
-	 * @layout 5,col-xs-4
-	 */
-	public $pid;
+    use JQueryValidator;
+    public $table = null;
+    /**
+     * 表名
+     * @var \backend\form\TextField
+     * @type string
+     * @required
+     * @layout 1,col-xs-4
+     */
+    public $tableName;
+    /**
+     * 值字段
+     * @var  \backend\form\TextField
+     * @type string
+     * @required
+     * @layout 1,col-xs-4
+     */
+    public $key = 'id';
+    /**
+     * 文本字段
+     * @var \backend\form\TextField
+     * @type string
+     * @required
+     * @layout 1,col-xs-4
+     */
+    public $fields = 'name';
+    /**
+     * 条件
+     * @var \backend\form\TextField
+     * @type string
+     * @layout 2,col-xs-12
+     */
+    public $where;
+    /**
+     * 解析条件中的变量
+     * @var \backend\form\CheckboxField
+     * @type bool
+     * @layout 3,col-xs-12
+     */
+    public $eval = 0;
+    /**
+     * 排序
+     * @var \backend\form\TextField
+     * @type string
+     * @note   格式: field a,field2 d
+     * @layout 4,col-xs-12
+     */
+    public $orderBy;
+    /**
+     * 默认选项提示文字
+     * @var \backend\form\TextField
+     * @type string
+     * @layout 5,col-xs-8
+     */
+    public $option;
+    /**
+     * 树型选项父字段
+     * @var \backend\form\TextField
+     * @type string
+     * @layout 5,col-xs-4
+     */
+    public $pid;
 }
