4b07a28eb1ce788abddb69c3ea2f7216cf5bda9f
windywany <windywany@163.com>
2019-04-12 11:58:53 +0800

    use protected modifier

--- wulaphp/auth/PassportSupport.php
+++ wulaphp/auth/PassportSupport.php
@@ -3,6 +3,7 @@
 namespace wulaphp\auth;
 
 use wulaphp\mvc\view\SmartyView;
+use wulaphp\mvc\view\ThemeView;
 
 /**
  * 用户通行证认证特性。添加此特性后，在控制器中可直接通过$passport属性访问当前用户的通行证.
@@ -10,28 +11,28 @@ use wulaphp\mvc\view\SmartyView;
  * 注：此特性依赖SessionSupport.
  *
  * @package wulaphp\auth
- * @property string $passportType
+ * @property-read  string $passportType
  */
 trait PassportSupport {
-	/**
-	 * @var Passport
-	 */
-	protected $passport;
+    /**
+     * @var Passport
+     */
+    protected $passport;
 
-	protected function onInitPassportSupport() {
-		if (isset($this->passportType)) {
-			$this->passport = Passport::get($this->passportType);
-		} else {
-			trigger_error('not set passportType property in Controller:' . get_class($this) . ', use default passport', E_USER_WARNING);
-			$this->passport = Passport::get();
-		}
-	}
+    protected final function onInitPassportSupport() {
+        if (isset($this->passportType)) {
+            $this->passport = Passport::get($this->passportType);
+        } else {
+            trigger_error('passportType property not found in Controller:' . get_class($this) . ', use default passport', E_USER_WARNING);
+            $this->passport = Passport::get();
+        }
+    }
 
-	public function afterRunInPassportSupport($action, $view, $method) {
-		if ($view instanceof SmartyView) {
-			$view->assign('myPassport', $this->passport);
-		}
+    protected function afterRunInPassportSupport($action, $view, $method) {
+        if ($view instanceof SmartyView || $view instanceof ThemeView) {
+            $view->assign('myPassport', $this->passport);
+        }
 
-		return $view;
-	}
+        return $view;
+    }
 }
