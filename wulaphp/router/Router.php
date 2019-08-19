2a96ffbb32468ca93f05c13e276359783abbc56c
windywany <windywany@163.com>
2019-08-19 09:51:21 +0800

    仅运行在cli-server时注册静态资源分发器

--- wulaphp/router/Router.php
+++ wulaphp/router/Router.php
@@ -50,7 +50,10 @@ class Router {
         //路由表分发器
         $this->register(new RouteTableDispatcher (), 2);
         fire('router\registerDispatcher', $this);
-        $this->register(new ModuleResDispatcher(), 1000);
+        // 仅以开发服务器运行时才需要注册静态资源分发器
+        if (PHP_SAPI == 'cli-server') {
+            $this->register(new ModuleResDispatcher(), 1000);
+        }
     }
 
     /**
