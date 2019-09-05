eb36ca33d5c7327351107d5e8376217c3ff42204
windywany <windywany@163.com>
2019-09-05 11:56:13 +0800

    optimize plugin

--- tests/modules/login/hooks/PassportNewViptestPassportAlter.php
+++ tests/modules/login/hooks/PassportNewViptestPassportAlter.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace tests\modules\login\hooks;
+namespace login\hooks;
 
 use login\classes\VipTestPassport;
 use wulaphp\hook\Alter;
