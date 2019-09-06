6a4df8b9eed9b4f41be2444f47e9f64922affefa
windywany <windywany@163.com>
2019-09-07 07:25:32 +0800

    add missing service_config.php

---
+++ tests/conf/service_config.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * This file is part of wulacms.
+ *
+ * (c) Leo Ning <windywany@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'verbose'  => 'vvvv',
+    'services' => [
+        'strrev'  => [
+            'type'        => 'gearman',
+            'job'         => 'strrev',
+            'json'        => false,
+            'workerClass' => 'app\gearman\ReverseWorker'
+        ],
+        'strrev2' => [
+            'type'   => 'gearman',
+            'job'    => 'strrev2',
+            'json'   => false,
+            'script' => 'modules/app/script/strrev.php'
+        ]
+    ]
+];
