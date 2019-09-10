f0ff474604eee785555a7aa23c99557110e01bbb
windywany <windywany@163.com>
2019-09-10 10:54:13 +0800

    remove unused imports

--- bin/_/modules/home/bootstrap.php
+++ bin/_/modules/home/bootstrap.php
@@ -6,17 +6,21 @@ use wulaphp\app\App;
 use wulaphp\app\Module;
 
 class HomeModule extends Module {
-	public function getName() {
-		return '默认首页';
-	}
+    public function getName() {
+        return '默认首页';
+    }
 
-	public function getDescription() {
-		return '处理首页请求，可根据需要修改此模块以满足您的需求';
-	}
+    public function getDescription() {
+        return '处理首页请求，可根据需要修改此模块以满足您的需求';
+    }
 
-	public function getHomePageURL() {
-		return 'http://www.wulaphp.com/';
-	}
+    public function getHomePageURL() {
+        return '';
+    }
+
+    public function getAuthor() {
+        return 'wulaphp dev team';
+    }
 }
 
 App::register(new HomeModule());
--- bin/_/modules/home/controllers/IndexController.php
+++ bin/_/modules/home/controllers/IndexController.php
@@ -2,14 +2,12 @@
 
 namespace home\controllers;
 
-use wulaphp\io\Response;
 use wulaphp\mvc\controller\Controller;
-use wulaphp\router\Router;
 
 class IndexController extends Controller {
 
-	public function index() {
-		// 可以按需修改
-		return template('index.tpl', ['nihao' => 'Hello wula']);
-	}
+    public function index() {
+        // 可以按需修改
+        return template('index.tpl', ['nihao' => 'Hello wula']);
+    }
 }
