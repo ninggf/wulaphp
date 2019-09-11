99bc848ce78392a388d9741cf9ee499809d95f9a
windywany <windywany@163.com>
2019-09-11 20:20:56 +0800

    use LF instead on CRLF

--- bin/_/conf/config.php
+++ bin/_/conf/config.php
@@ -3,9 +3,9 @@
  * 应用配置文件.
  */
 return [
-	'debug'    => env('debug', DEBUG_DEBUG),
-	'resource' => [
-		'combinate' => env('resource.combinate', 0),
-		'minify'    => env('resource.minify', 0)
-	]
+    'debug'    => env('debug', DEBUG_DEBUG),
+    'resource' => [
+        'combinate' => env('resource.combinate', 0),
+        'minify'    => env('resource.minify', 0)
+    ]
 ];
