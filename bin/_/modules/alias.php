f224f5a58caa62489e3b29d68a8d44ea4bb1683f
windywany <windywany@163.com>
2019-09-23 13:07:13 +0800

    give a example for ailas

--- bin/_/modules/alias.php
+++ bin/_/modules/alias.php
@@ -1,13 +1,5 @@
 <?php
-/*
- * This file is part of wulacms.
- *
- * (c) Leo Ning <windywany@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-// your alias here
-return [
-    'sub' => 'testm/sub'
-];
+// your alias here, something like follow:
+// ['login.html'=>'user/login']
+
+return [];
