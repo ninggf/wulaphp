68864403fb20eaa7e0c7db4501ccc17f5a682d53
windywany <windywany@163.com>
2019-10-08 16:25:00 +0800

    add serve command

--- includes/kernelimpl.php
+++ includes/kernelimpl.php
@@ -2,6 +2,7 @@
 
 use wulaphp\command\AdminCommand;
 use wulaphp\command\CreateComamnd;
+use wulaphp\command\ServeCommand;
 
 bind('get_redis_cache', function ($cache, $cfg) {
     if (!$cache) {
@@ -22,6 +23,7 @@ bind('get_memcached_cache', function ($cache, $cfg) {
 bind('artisan\getCommands', function ($cmds) {
     $cmds['admin']  = new AdminCommand();
     $cmds['create'] = new CreateComamnd();
+    $cmds['serve']  = new ServeCommand();
 
     if (function_exists('pcntl_fork') && function_exists('posix_getpid')) {
         if (function_exists('socket_create')) {
---
+++ wulaphp/command/ServeCommand.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * This file is part of wulacms.
+ *
+ * (c) Leo Ning <windywany@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace wulaphp\command;
+
+use wulaphp\artisan\ArtisanCommand;
+
+class ServeCommand extends ArtisanCommand {
+    private $proc;
+    private $descriptorspec;
+
+    public function cmd() {
+        return 'serve';
+    }
+
+    public function desc() {
+        return 'run php built-in Development server';
+    }
+
+    protected function argDesc() {
+        return '[[host:]port]';
+    }
+
+    protected function execute($options) {
+        $opt = $this->opt(0);
+        $cmd = escapeshellcmd(PHP_BINARY);
+
+        if ($opt) {
+            $arg = escapeshellarg($opt) . ' index.php';
+        } else {
+            $arg = escapeshellarg('127.0.0.1:8080') . ' index.php';
+        }
+
+        $this->proc           = $cmd . ' -S ' . $arg;
+        $this->descriptorspec = [
+            0 => STDIN,
+            1 => STDOUT,
+            2 => STDERR
+        ];
+
+        if (($r = proc_open($this->proc, $this->descriptorspec, $pipes, WWWROOT))) {
+            while (true) {
+                sleep(5);
+                $status = proc_get_status($r);
+                if (!$status || !$status['running']) {
+                    break;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
