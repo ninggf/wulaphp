9c3fc6741a83fa17560f72a4e2f228d8efefd5e8
windywany <windywany@163.com>
2020-09-16 09:56:45 +0800

    使用本地测试服务器

--- tests/Tests/util/CurlientTest.php
+++ tests/Tests/util/CurlientTest.php
@@ -25,11 +25,11 @@ class CurlientTest extends TestCase {
 
     public function testWrite() {
         $client = CurlClient::getClient(20);
-        $url    = 'http://46.push2.eastmoney.com/api/qt/stock/sse?ut=fa5fd1943c7b386f172d6893dbfba10b&fltt=2&fields=f50,f48,f117&secid=0.002703';
+        $url    = 'http://127.0.0.1:9090/admin';
 
         $client->get($url, function ($ch, $data) use (&$rtn) {
-            preg_match('/^data: (.+)/', $data, $m);
-            $rtn = @json_decode($m[1], true);
+            preg_match('/^admin(.+?)ok$/', $data, $m);
+            $rtn = $m[1];
 
             return 0;
         });
